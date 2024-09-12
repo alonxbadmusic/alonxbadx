df95e0436150ef956f8cf2c14fa8a8461a136644
EXPERT CODER <150228624+TMM-TEAM@users.noreply.github.com>
2024-09-13 01:07:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","BADLIAR2001")
+OWNER_USERNAME = getenv("OWNER_USERNAME","expertcoderr")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME" , "CuteshizuBot")
 # Don't Add style font 
@@ -29,7 +29,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1001801976314))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5679673719)
+OWNER_ID = int(getenv("OWNER_ID", 6656608288)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
